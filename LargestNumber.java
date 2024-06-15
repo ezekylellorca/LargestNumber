70267b8046cdd2c967f67ca0cf36014d7c0dc90c
Eze <ezekylellorca@users.noreply.github.com>
2024-06-15 14:46:42 +0800

    Update LargestNumber.java

--- LargestNumber.java
+++ LargestNumber.java
@@ -4,13 +4,13 @@ public class LargestNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter the first number: ");
+        System.out.print("Enter the 1st Number: ");
         int num1 = scanner.nextInt();
 
-        System.out.print("Enter the second number: ");
+        System.out.print("Enter the 2nd Number: ");
         int num2 = scanner.nextInt();
 
-        System.out.print("Enter the third number: ");
+        System.out.print("Enter the 3rd Number: ");
         int num3 = scanner.nextInt();
 
         scanner.close();
@@ -28,7 +28,7 @@ public class LargestNumber {
                 largest = num3;
             }
 
-            System.out.println("The largest number is: " + largest);
+            System.out.println("The Largest Number is: " + largest);
         }
     }
 }
